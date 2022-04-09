072d06f8492a80a8f8c2c054871eeb9fbc9afae0
nMDaas <63910563+nMDaas@users.noreply.github.com>
2022-04-09 15:23:45 -0400

    view Recommended Recipes

--- bakeryapp.sql
+++ bakeryapp.sql
@@ -30,14 +30,19 @@ DROP TABLE dessertType;
 CREATE TABLE dessertType (
    typeId INT AUTO_INCREMENT,   
    typeName enum('cakes', 'cupcakes', 'pudding', 'biscuits', 'pastry', 'bread'),
-   typeDescription VARCHAR (120),
+   typeDescription VARCHAR (1500),
    PRIMARY KEY(typeId) 
 );
 
 INSERT INTO     
 dessertType(typeId, typeName, typeDescription)    
 VALUES 
-(1, 'pastry', "An item of food consisting of sweet pastry with a cream, jam, or fruit filling");
+(1, 'cakes', "An item of soft, sweet food made from a mixture of flour, shortening, eggs, sugar, and other ingredients, baked and often decorated."), 
+(2, 'cupcakes', "A small cake baked in a cup-shaped container and typically iced."),
+(3, 'pudding', "A flavored, custard-like dessert made of milk, sugar, and a thickening agent such as egg yolks or corn starch."),
+(4, 'biscuits', "A small, typically round cake of bread leavened with baking powder, baking soda, or sometimes yeast."),
+(5, 'pastry', "An item of food consisting of sweet pastry with a cream, jam, or fruit filling."),
+(6, 'bread', "Food made of flour, water, and yeast or another leavening agent, mixed together and baked");
 
 DROP TABLE Recipe;
 CREATE TABLE Recipe (
@@ -56,12 +61,27 @@ Recipe(recipeId, recipeName, instructions, type, writer)
 VALUES 
 (1, "3-Step Strawberry Dessert", "Cut 2 sheets of puff pastry in half. Bake 3 of them at 400°F Just 4 ingredients! for 18 minutes. 
 In a medium bowl, whip 1 cup crème fraîche and 2 Tbsp. confectioners' sugar until fluffy.
-Layer pastry, cream and 4 oz. strawberries; repeat. Dust with confectioners' sugar. Serve.", 1, "Sweet Magnolias"),
+Layer pastry, cream and 4 oz. strawberries; repeat. Dust with confectioners' sugar. Serve.", 5, "Sweet Magnolias"),
 (2, "Cake & Berry Campfire Cobbler", "Prepare grill or campfire for low heat, using 16-20 charcoal briquettes or large wood chips.
 Line an ovenproof Dutch oven with heavy-duty aluminum foil; add pie filling. In a large bowl, combine the cake mix, water and oil. Spread over pie filling.
 Cover Dutch oven. When briquettes or wood chips are covered with white ash, place Dutch oven directly on top of 8-10 of them. Using long-handled tongs, place remaining briquettes on pan cover.
 Cook until filling is bubbly and a toothpick inserted in the topping comes out clean, 30-40 minutes. To check for doneness, use the tongs to carefully lift the cover. If desired, serve with ice cream.", 
-1, "Sweet Magnolias");
+5, "Sweet Magnolias"), 
+(3, "Microwave Chocolate Mug Cake", "Mix flour, sugar, cocoa powder, baking soda, and salt in a large microwave-safe mug; stir in milk, canola oil, water, and vanilla extract.
+Cook in microwave until cake is done in the middle, about 1 minute 45 seconds.", 
+1, "Sweet Magnolias"), 
+(4, "Chocolate Cupcakes", "Preheat oven to 350 degrees F (175 degrees C). Grease two muffin pans or line with 20 paper baking cups.
+In a medium bowl, beat the butter and sugar with an electric mixer until light and fluffy. Mix in the eggs, almond extract and vanilla. Combine the flour, cocoa, baking powder and salt; stir into the batter, alternating with the milk, just until blended. Spoon the batter into the prepared cups, dividing evenly.
+Bake in the preheated oven until the tops spring back when lightly pressed, 20 to 25 minutes. Cool in the pan set over a wire rack. When cool, arrange the cupcakes on a serving platter. Frost with your favorite frosting.", 
+2, "Sweet Magnolias"), 
+(5, "Vanilla Pudding", "Mix sugar, cornstarch, and salt into a saucepan. Gradully stir in milk.
+Bring to a boil over medium heat. Boil 1 minute, sitrring constantly.
+Remove from heat. Stir in butter and vanilla. Chill.", 
+3, "Sweet Magnolias"), 
+(6, "3-Ingredient Peanut Butter Cookies", "Preheat oven to 350 degrees F (175 degrees C).
+Mix peanut butter, sugar, and egg together in a bowl using an electric mixer until smooth and creamy. Roll mixture into small balls and arrange on a baking sheet; flatten each with a fork, making a criss-cross pattern.
+Bake in the preheated oven for 10 minutes. Cool cookies on the baking sheet for 2 minutes before moving to a plate.", 
+4, "Sweet Magnolias");
 
 CREATE TABLE Ingredient (
    ingredientId INT AUTO_INCREMENT,   
@@ -90,6 +110,16 @@ CREATE TABLE userDessertType (
    CONSTRAINT `fk_dessert_type` FOREIGN KEY (`dessertType`) REFERENCES `dessertType` (`typeId`)
 );
 
+INSERT INTO     
+userDessertType(dessertType, user)    
+VALUES 
+(1, "Caroline"),
+(3, "Caroline"),
+(5, "Caroline"),
+(2, "Natasha"),
+(4, "Natasha"),
+(5, "Natasha");
+
 CREATE TABLE writtenRecipe (
    recipe INT,
    writer VARCHAR(64),
@@ -114,6 +144,7 @@ CREATE TABLE inventoryIngredient (
    CONSTRAINT `fk_inventory` FOREIGN KEY (`inventory`) REFERENCES `Inventory` (`inventoryId`)
 );
 
+DROP TABLE saves;
 CREATE TABLE saves (
    user VARCHAR(64),
    inventory INT,
@@ -131,7 +162,7 @@ VALUES
 ("Caroline", 2, 1, false);
 
 
--- Check if a username exists
+-- FUCTION: Check if a username exists
 DROP FUNCTION IF EXISTS usernameExists;
 DELIMITER //
 CREATE FUNCTION usernameExists
@@ -152,10 +183,10 @@ RETURN(name) ;
 END//
 DELIMITER ;
 
--- testing function
+-- TESTS: testing function
 SELECT usernameExists("xyz");
 
--- view saved recipes 
+-- PROCEDURE: view saved recipes 
 DROP PROCEDURE viewSavedRecipes;
 DELIMITER //
 CREATE PROCEDURE viewSavedRecipes(user VARCHAR(64))
@@ -170,5 +201,24 @@ BEGIN
 END //
 DELIMITER ;
 
--- testing procedure
+-- TESTS: testing procedure
 CALL viewSavedRecipes("Caroline");
+
+-- PROCEDURE: view recommended recipes
+DROP PROCEDURE viewRecommendedRecipes;
+DELIMITER //
+CREATE PROCEDURE viewRecommendedRecipes(user VARCHAR(64))
+BEGIN
+	 SELECT DISTINCT(recipeName), instructions, dessertType.typeName, writer
+		FROM Recipe LEFT OUTER JOIN userDessertType  
+        ON userDessertType.dessertType = Recipe.type
+        JOIN dessertType
+        ON dessertType.typeId = Recipe.type
+        WHERE userDessertType.user = user;
+        
+END //
+DELIMITER ;
+
+-- TESTS: testing procedure
+CALL viewRecommendedRecipes("Caroline");
+
